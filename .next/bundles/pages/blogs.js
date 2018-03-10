ea76753179de64dd2ea45e842277c6161a3409d9
Jair Alexander Escobar <jairescobar@MacBook-Pro-de-Jair.local>
2018-03-10 12:28:30 -0500

    Inicialización de una simple app con Next.js

---
+++ .next/bundles/pages/blogs.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/blogs', function() {
+            var comp = module.exports=webpackJsonp([8],{235:function(e,t,l){e.exports=l(236)},236:function(e,t,l){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=l(1),r=u(n),o=l(38),a=u(o);t.default=function(e){return r.default.createElement(a.default,null,r.default.createElement("h1",null,e.url.query.title),r.default.createElement("p",null,"This is the blog post content."))}}},[235]);
+            return { page: comp.default }
+          })
+        
