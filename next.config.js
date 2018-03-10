5824c1d01f9941234807363ddc3fd0cae30939c0
JairEscobar <evitalires@gmail.com>
2018-03-10 16:52:17 -0500

    Terminando de arreglar las rutas en next.config y Header.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     return {
       '/': { page: '/' },
       '/blog': { page: '/blog' },
+      '/markDown': { page: 'markDown'},
       '/blogs/hello-nextjs': { page: '/blogs', query: { title: "Hello Next.js" } },
       '/blogs/learn-nextjs': { page: '/blogs', query: { title: "Learn Next.js is awesome" } },
       '/blogs/deploy-nextjs': { page: '/blogs', query: { title: "Deploy apps with Zeit" } },
